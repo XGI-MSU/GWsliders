a1f5ccb36acbb66b79661c5efae4668ac25e271f
piper-morris <pipermorris5603@gmail.com>
2025-11-14 11:38:11 -0700

    'dpi change'

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ plt.close('all')
 
 # setup main plot
 #fig, ax = plt.subplots(figsize=(5, 2), dpi= 150)
-fig, ax = plt.subplots(figsize=(7, 4), dpi= 100)
+fig, ax = plt.subplots(figsize=(7, 4), dpi= 105)
 
 # adjust plot area
 fig.subplots_adjust(left=0.3, bottom=0.32, right=0.95, top=0.97)
